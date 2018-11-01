157760d8feec99bbdc19a16adf7220706b296dd6
taylorrgriffin <Taylor.Griffin2@oregonstate.edu>
2018-11-01 12:52:49 -0700

    Removed metadata from models. Adding test data to db.

--- PeopleProTraining/PeopleProTraining.Dal/Models/Building.cs
+++ PeopleProTraining/PeopleProTraining.Dal/Models/Building.cs
@@ -1,21 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PeopleProTraining.Dal.Models
 {
-    [MetadataType(typeof(BuildingMetaData))]
     public partial class Building
     {
-    }
-    public class BuildingMetaData
-    {
         public int BuildingID;
-
-        [Required]
         public string BuildingName;
         public ICollection<Department> Departments;
     }
--- PeopleProTraining/PeopleProTraining.Dal/Models/Employee.cs
+++ PeopleProTraining/PeopleProTraining.Dal/Models/Employee.cs
@@ -1,23 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PeopleProTraining.Dal.Models
 {
-    [MetadataType(typeof(EmployeeMetaData))]
     public partial class Employee
     {
-    }
-    public class EmployeeMetaData
-    {
         public int EmployeeID;
-
-        [Required]
-        public string FirstName;
+        public string First_Name;
         public string LastName;
-        public Department Department; 
+        public Department Department;
     }
 }
